c0ac6915dcd27beb1c2f4df31ba979d3886c36fb
Young wook Jeon <ela87@naver.com>
2021-01-07 20:22:01 +0900

    Add Service for Reply

--- board/src/main/java/com/young/board/repository/ReplyRepository.java
+++ board/src/main/java/com/young/board/repository/ReplyRepository.java
@@ -1,13 +1,18 @@
 package com.young.board.repository;
 
+import com.young.board.entity.Board;
 import com.young.board.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     @Modifying // JPQL 사용하여 update or delete 실행하기 위함
     @Query("delete from Reply r where r.board.bno = :bno")
     void deleteByBno(Long bno);
+
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
---
+++ board/src/test/java/com/young/board/service/ReplyServiceTests.java
@@ -0,0 +1,22 @@
+package com.young.board.service;
+
+import com.young.board.dto.ReplyDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class ReplyServiceTests {
+
+    @Autowired
+    private ReplyService service;
+
+    @Test
+    public void testGetList() {
+        Long bno = 100L;
+        List<ReplyDTO> replyDTOList = service.getList(bno);
+        replyDTOList.forEach(System.out::println);
+    }
+}
